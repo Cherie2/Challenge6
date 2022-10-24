9f8cf415138a302a43c0a2a1861f58a0bc33a042
Cherie2 <cwalk280@yahoo.com>
2022-10-24 16:54:38 -0700

    Revised CSS and index structure

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,20 @@
 var weatherAPIKey = "0f87f7930ed9ae3d9a321c614c64f22c";
 var inputCity = document.getElementById('citySearch');
-var citySearchBtn = document.getElementById('search')
+var citySearchBtn = document.getElementById('search');
+var inputHistory=document.querySelector("#search-history");
 //need three API from openweather
-function handleUserInput(){
-var userinput = inputCity.value;
 
-getWeather(userinput);
+//Function to respond to user city search and create buttons with past city searches for later use
+function handleUserInput(){
+    var userInput = inputCity.value;
+    var userHistory=document.createElement("button");
+    userHistory.textContent=userInput;
+    inputHistory.append(userHistory);
+    getWeather(userInput);
 }
 
 function getWeather(city) {
+    var city= "riverside";
     var currentWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + weatherAPIKey;
    fetch(currentWeatherURL)
         .then(function (response) {
@@ -18,12 +24,21 @@ function getWeather(city) {
         console.log(data);
          });
 }
-
-
 function displayWeather(data) {
-    
-    
+    var currentDay =[ 
+     {sunrise, sunset} = data.city,
+     {icon, description} = data.weather[0],
+     {temp, humidity}= data.main,
+     {speed}= data.wind,
+    ]
+    console.log(currentDay);
+    var futureDay = [
+    {city, sunrise, sunset} = data.city,
+    {icon, description} = data.weather[0],
+    {temp, humidity}= data.main,
+    {speed}= data.wind,
+    ]
 }
 
-
+getWeather();
 citySearchBtn.addEventListener('click', handleUserInput)
